da1d330f145f33cd9e4ec8ae615a4fa7e7c9cdc7
Avrohom Yisroel Silver <mryossu@hotmail.com>
2024-06-28 18:20:38 +0100

    Added a NextSelect method that works exactly like Next, but is all expressions. Bit messy though

--- Life.Game/LifeHelpers.cs
+++ Life.Game/LifeHelpers.cs
@@ -1,4 +1,6 @@
-﻿namespace Life.Game;
+﻿using System.Data;
+
+namespace Life.Game;
 
 public static class LifeHelpers {
   /// <summary>
@@ -17,12 +19,16 @@ public static class LifeHelpers {
         result[i, j] = source[i * dim2 + j];
       }
     }
+
     return result;
   }
 
   public static int Count(bool[,] board, int x, int y) =>
     new[] { -1, 0, 1, -1, 1, -1, 0, 1 }.Zip(new[] { 1, 1, 1, 0, 0, -1, -1, -1 })
-      .Sum(o => (x + o.First >= 0) && (x + o.First < board.GetLength(0)) && (y + o.Second >= 0) && (y + o.Second < board.GetLength(1) && board[x + o.First, y + o.Second]) ? 1 : 0);
+      .Sum(o => (x + o.First >= 0) && (x + o.First < board.GetLength(0)) && (y + o.Second >= 0) &&
+                (y + o.Second < board.GetLength(1) && board[x + o.First, y + o.Second])
+        ? 1
+        : 0);
 
   /*
     At each step in time, the following transitions occur:
@@ -49,7 +55,7 @@ public static class LifeHelpers {
         f    6      f
         f    7      f
         f    8      f
-        
+
         So cell is alive if...
         t    2      t
         t    3      t
@@ -61,21 +67,11 @@ public static class LifeHelpers {
   public static bool[,] Next(this bool[,] board) =>
     board.Cast<bool>().ToArray()
       .Select((cell, pos) => {
-        (int x, int y) = PosToCoords(board, pos);
-        int count = Count(board, x, y);
-
-        // ie when count==2 and cell, or when count==3
-        //return (count == 2 && cell) || count == 3;
-
+        (int row, int col) = PosToCoords(board, pos);
+        int count = Count(board, row, col);
         // General logic that can handle any Life rules. Eventually we will pass these in as parameters, derived from a string, eg B3/S23
-        int[] survives = [2,3]; // Any live cell with this number of live neighbours is alive in the next generation
         int[] born = [3]; // Any dead cell with this number of live neighbours becomes alive in the next generation
-
-        bool res = cell switch {
-          true when survives.Contains(count) => true,
-          false when born.Contains(count) => true,
-          _ => false
-        };
+        int[] survives = [2, 3]; // Any live cell with this number of live neighbours is alive in the next generation
         return cell switch {
           true when survives.Contains(count) => true,
           false when born.Contains(count) => true,
@@ -85,9 +81,26 @@ public static class LifeHelpers {
       .ToArray()
       .To2D(board.GetLength(0));
 
+  public static bool[,] NextSelect(this bool[,] board) =>
+    NextSelect(board, [3], [2, 3]);
+
+  public static bool[,] NextSelect(this bool[,] board, int[] born, int[] survives) =>
+    board.Cast<bool>().ToArray()
+      .Select((cell, pos) => new { cell, coord = PosToCoords(board, pos) })
+      .Select(data => new { data.cell, data.coord.row, data.coord.col })
+      .Select(data => new { data.cell, data.row, data.col, count = Count(board, data.row, data.col) })
+      .Select(data => data.cell switch {
+        true when survives.Contains(data.count) => true,
+        false when born.Contains(data.count) => true,
+        _ => false
+      })
+      .ToArray()
+      .To2D(board.GetLength(0));
+
+
   // [2,3] is 2 rows x 3 cols
   // board.GetLength(0) is rows
   // board.GetLength(1) is cols
-  public static (int row, int cols) PosToCoords(bool[,] board, int pos) =>
+  public static (int row, int col) PosToCoords(bool[,] board, int pos) =>
     (pos / board.GetLength(1), pos % board.GetLength(1));
 }
