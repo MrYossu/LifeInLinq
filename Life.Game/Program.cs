19fd84971813a8d164528d3deb6632c47dfd6eeb
Avrohom Yisroel Silver <mryossu@hotmail.com>
2024-07-02 21:36:59 +0100

    Added some timing tests and code to run a variation on the regular rules

--- Life.Game/Program.cs
+++ Life.Game/Program.cs
@@ -1,14 +1,31 @@
-﻿using Life.Game;
+﻿using System.Diagnostics;
+using Life.Game;
 using static Life.Game.GameHelpers;
 using static Life.Game.LifeHelpers;
 
-bool[,] board = InitialiseRandom(50, 200);
+bool[,] board = InitialiseRandom(50, 100);
 Console.WriteLine("Press any key to start");
 Console.ReadKey();
 Console.Clear();
 while (true) {
   Console.WriteLine(board.ToBoardString(true));
-  board = board.NextSelect("B3/S23");
+  board = board.NextSelect("B1/S12");
   Thread.Sleep(400);
   Console.Clear();
 }
+
+/*
+// The automaton B1/S12 generates four very close approximations to the Sierpinski triangle when applied to a single live cell
+bool[,] board = InitialiseEmpty(50, 100);
+board[25, 50] = true;
+*/
+
+/*
+// Timing test
+Stopwatch sw = Stopwatch.StartNew();
+for (int i = 0; i < 1000; i++) {
+  board = board.NextSelect();
+}
+Console.WriteLine(sw.ElapsedMilliseconds);
+return;
+*/
